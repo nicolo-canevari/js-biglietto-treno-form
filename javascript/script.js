b5a5214e4e43267998bf240418da02d0fb1fc77a
nicolo_canevari <nico.canevari@outlook.it>
2025-01-13 15:26:42 +0100

    [feat] JS sconti in base all'età del passeggero

--- javascript/script.js
+++ javascript/script.js
@@ -1,6 +1,22 @@
 
-   // Prezzo base al km
-   const prezzoBase = 0.21;
+// Prezzo base al km
+const prezzoBase = 0.21;
 
-   // Calcolo il prezzo iniziale senza sconto
-   let prezzoTotale = km * prezzoBase;
+// Calcolo il prezzo iniziale senza sconto
+let prezzoTotale = km * prezzoBase;
+
+// Sconti in base all'età
+if (age < 18) {
+
+    // Sconto del 20% per i minorenni
+    prezzoTotale *= 0.8;
+
+} else if (age > 65) {
+
+    // Sconto del 40% per gli over 65
+    prezzoTotale *= 0.6;
+    
+}
+
+// Mostriamo il risultato nella console
+console.log(`Il prezzo totale del biglietto è: €${prezzoTotale.toFixed(2)}`);
