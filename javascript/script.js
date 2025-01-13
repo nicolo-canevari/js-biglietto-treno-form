bbadf2a0994b9a55acec4c91ea56e673075ee85f
nicolo_canevari <nico.canevari@outlook.it>
2025-01-13 15:38:23 +0100

    [feat] JS function calcolaPrezzo + stampa risultato nella console

--- javascript/script.js
+++ javascript/script.js
@@ -1,3 +1,9 @@
+// funzione per calcolare il prezzo del biglietto in base ai dati inseriti dall'utente
+function calcolaPrezzo() {
+
+    // Salvo i valori inseriti dall'utente
+    const km = parseFloat(document.getElementById('km').value);
+    const age = parseInt(document.getElementById('age').value);
 
 // Prezzo base al km
 const prezzoBase = 0.21;
@@ -15,8 +21,10 @@ if (age < 18) {
 
     // Sconto del 40% per gli over 65
     prezzoTotale *= 0.6;
-    
+
 }
 
-// Mostriamo il risultato nella console
+// Risultato nella console
 console.log(`Il prezzo totale del biglietto è: €${prezzoTotale.toFixed(2)}`);
+
+}
